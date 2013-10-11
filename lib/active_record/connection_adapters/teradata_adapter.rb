858fca0cd77f1cb5c53a4d6fe20dbc5146120e44
jc34 <jc34@uk.tesco.com>
2013-10-11 17:46:21 +0100

    implemented tables method

--- lib/active_record/connection_adapters/teradata_adapter.rb
+++ lib/active_record/connection_adapters/teradata_adapter.rb
@@ -3,6 +3,12 @@ require 'active_record/connection_adapters/abstract_adapter'
 #gem 'teradata-cli', :path => '~/projects/teradata-cli'
 require 'teradata-cli'
 
+class ActiveRecord::Base
+  def self.table_name_prefix
+    $teradata_table_name_prefix
+  end
+end
+
 module ActiveRecord
   module ConnectionHandling
     def teradata_connection(config)
@@ -23,6 +29,8 @@ module ActiveRecord
       def initialize(logger, logon_string, config)
         @logon_string = logon_string
         @charset = config[:charset]
+        @database = config[:database]
+        $teradata_table_name_prefix = config[:table_name_prefix]
         @config = config
         connect
         super @connection, logger
@@ -38,9 +46,27 @@ module ActiveRecord
       private :connect
 
       def tables(name = nil)
-        # FIXME: fetch metadata from database
-        ['shohins']
+        sql = "SELECT TABLENAME FROM DBC.TABLES "
+        clauses = []
+        clauses << "DATABASENAME = '#{@database}'" if @database
+        clauses << "TABLENAME = '#{name}'" if name
+        clauses << "TABLENAME LIKE '#{$teradata_table_name_prefix}%'" if $teradata_table_name_prefix
+        unless clauses.empty?
+          sql << " WHERE " + clauses.join(' AND ')
+        end
+        rs = execute(sql)
+        rs.entries.collect {|record| record['TableName'].strip}
       end
+
+      # Executes the SQL statement in the context of this connection.
+      def execute(sql, name = nil)
+        if name == :skip_logging
+          @connection.query(sql)
+        else
+          log(sql, name) { @connection.query(sql) }
+        end
+      end
+
     end
   end
 end
